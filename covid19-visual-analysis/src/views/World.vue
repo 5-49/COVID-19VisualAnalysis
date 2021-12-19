a3f1ecae7ab1bd020a027c0bfb5f2682f8b241fe
jin-xiangwen <72434626+jin-xiangwen@users.noreply.github.com>
2021-12-19 12:34:40 +0800

    新闻界面

--- covid19-visual-analysis/src/views/World.vue
+++ covid19-visual-analysis/src/views/World.vue
@@ -36,9 +36,26 @@
       </el-container>
 
       <!-- 右边：新闻 -->
-      <el-container style="width: 25%; height: 700px; background-color: black;  color: white">
-      
-      </el-container>
+      <div class="container">
+        <span class="newstitle">实时播报</span>
+        <div v-for="(item,index) in list" :key="index">
+          <div class="newsList" v-for="(onenew,index) in item.news" :key="index">
+            <div class="dateList">
+              <div style="text-align: center; font-size: 14px;">{{onenew.pubDateStr}}</div>
+              <div style="text-align: center; font-size: 12px;">{{onenew.pubDate}}</div>
+              <span class="dateItem"></span>
+            </div>
+            <div class="newsRight">
+              <div class="newsItemTitle">
+                <span class="newest" v-show="index==0">最新</span>
+                {{onenew.title}}
+              </div>
+              <a :href="onenew.sourceUrl" target="__blank" style="font-size: 12px;">{{onenew.summary}}</a>
+              <div style="text-align: right; font-size: 14px;">信息来源：{{onenew.infoSource}}</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </el-container>
 
@@ -86,6 +103,7 @@
 import {earthBaseTexture} from '../assets/earthBaseTexture.js'
 import {nameMap} from '../assets/world.json'
 import {dataArr} from '../assets/world.json'
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -120,11 +138,15 @@ export default {
       top10Data: {
         
       },
-      selectedCountry: ''
+      selectedCountry: '',
+
+      //新闻数据
+      list: Array
     }
   },
   mounted() {
     this.initData()
+    this.getNews()
   },
   methods: {
     handleRowClick(row) {
@@ -477,7 +499,20 @@ export default {
         myChart.resize()
       })
     },
-
+    //获取新闻数据
+    getNews () {
+      axios({
+        url: 'http://api.tianapi.com/ncov/index',
+        method: 'get',
+        params:{
+          key: '2f3865d193ed10d014d38fd729aa172a'
+        }
+      })
+      .then(res => {
+        this.list=res.data.newslist
+        console.log(this.list)
+      })
+    }
   },
   watch: {},
   created() {}
@@ -537,4 +572,99 @@ h {
     #d9db6b
   #2cf503
 }
+
+a {
+  text-decoration: none;
+  color: white;
+}
+.container {
+  width: 25%;
+  height: calc(100% - 60px);
+  position: fixed;
+  top: 80px;
+  bottom: 0;
+  right: 0;
+  background: #152c4c99;
+  color: white;
+  z-index: 1;
+  overflow-y: auto;
+}
+.newstitle {
+  display: inline-block;
+  width: 100%;
+  font-size: 18px;
+  height: 40px;
+  line-height: 40px;
+  font-weight: bold;
+  padding-left: 20px;
+}
+.newsList {
+  position: relative;
+  display: flex;
+}
+.dateList {
+  width: 30%;
+  flex: none;
+  transform: translateY(0.08rem);
+  border-right: 1px solid #ebebeb;
+}
+.dateItem {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: wheat;
+  position: absolute;
+  right: -5px;
+  top: -2px;
+  border-radius: 50%;
+}
+.newsItemTitle {
+  font-weight: bold;
+  font-size: 14px;
+}
+.newsRight {
+  width: 70%;
+  padding-left: 10px;
+  padding-bottom: 20px;
+  padding-right: 10px;
+}
+.newest {
+  display: inline-block;
+  width: 30px;
+  height: 20px;
+  background: red;
+  text-align: center;
+  border-radius: 2px;
+  line-height: 20px;
+}
+
+/* 滚动条样式开始 */
+/* ----chrome---- */
+::-webkit-scrollbar {
+  width: 4px;
+  height: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.2);
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 0;
+  background: rgba(0, 0, 0, 0.1);
+}
+
+/* ----chrome---- */
+/* ----firefox---- */
+* {
+  scrollbar-color: rgba(0, 0, 0, 0.2) rgba(0, 0, 0, 0.1);
+  scrollbar-width: thin;
+}
+
+/* ----firefox---- */
+/* edge、ie暂未找到解决方案，或者可以使用js库来进行优化 */
+/* 滚动条样式结束 */
 </style>
--- covid19-visual-analysis/src/views/demo.vue
+++ covid19-visual-analysis/src/views/demo.vue
@@ -10,6 +10,26 @@
         </div>
       </div>
     </div>
+    <div class="container">
+      <span class="newstitle">实时播报</span>
+      <div v-for="(item,index) in list" :key="index">
+        <div class="newsList" v-for="(onenew,index) in item.news" :key="index">
+          <div class="dateList">
+            <div style="text-align: center; font-size: 14px;">{{onenew.pubDateStr}}</div>
+            <div style="text-align: center; font-size: 12px;">{{onenew.pubDate}}</div>
+            <span class="dateItem"></span>
+          </div>
+          <div class="newsRight">
+            <div class="newsItemTitle">
+              <span class="newest" v-show="index==0">最新</span>
+              {{onenew.title}}
+            </div>
+            <a :href="onenew.sourceUrl" target="__blank" style="font-size: 12px;">{{onenew.summary}}</a>
+            <div style="text-align: right; font-size: 14px;">信息来源：{{onenew.infoSource}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -171,5 +191,98 @@ export default{
 </script>
 
 <style lang="less" scoped>
+a {
+  text-decoration: none;
+  color: white;
+}
+.container {
+  width: 15%;
+  height: calc(100% - 60px);
+  position: fixed;
+  top: 80px;
+  bottom: 0;
+  right: 0;
+  background: #152c4c99;
+  color: white;
+  z-index: 1;
+  overflow-y: auto;
+}
+.newstitle {
+  display: inline-block;
+  width: 100%;
+  font-size: 18px;
+  height: 40px;
+  line-height: 40px;
+  font-weight: bold;
+  padding-left: 20px;
+}
+.newsList {
+  position: relative;
+  display: flex;
+}
+.dateList {
+  width: 30%;
+  flex: none;
+  transform: translateY(0.08rem);
+  border-right: 1px solid #ebebeb;
+}
+.dateItem {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: wheat;
+  position: absolute;
+  right: -5px;
+  top: -2px;
+  border-radius: 50%;
+}
+.newsItemTitle {
+  font-weight: bold;
+  font-size: 14px;
+}
+.newsRight {
+  width: 70%;
+  padding-left: 10px;
+  padding-bottom: 20px;
+  padding-right: 10px;
+}
+.newest {
+  display: inline-block;
+  width: 30px;
+  height: 20px;
+  background: red;
+  text-align: center;
+  border-radius: 2px;
+  line-height: 20px;
+}
 
+/* 滚动条样式开始 */
+/* ----chrome---- */
+::-webkit-scrollbar {
+  width: 4px;
+  height: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.2);
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius: 0;
+  background: rgba(0, 0, 0, 0.1);
+}
+
+/* ----chrome---- */
+/* ----firefox---- */
+* {
+  scrollbar-color: rgba(0, 0, 0, 0.2) rgba(0, 0, 0, 0.1);
+  scrollbar-width: thin;
+}
+
+/* ----firefox---- */
+/* edge、ie暂未找到解决方案，或者可以使用js库来进行优化 */
+/* 滚动条样式结束 */
 </style>
