c0345261f40bdb4d555afac6bd6fb15c8d64a0dc
Mocha259 <3378681490@qq.com>
2021-12-28 13:37:55 +0800

    解决bug

--- covid19-visual-analysis/src/views/World.vue
+++ covid19-visual-analysis/src/views/World.vue
@@ -213,11 +213,16 @@ export default {
       this.globalInfo.totDead      += parseInt(this.updateInfo.newDead)
 
       var countryIndex = (this.allCountrys || []).findIndex(item => item.name === this.updateInfo.name)
-      this.eachCountryInfo[countryIndex].curConfirmed += this.updateInfo.newConfirmed
-      this.eachCountryInfo[countryIndex].totConfirmed += this.updateInfo.newConfirmed
-      this.eachCountryInfo[countryIndex].totDead      += this.updateInfo.newDead
+      this.eachCountryInfo[countryIndex].curConfirmed += parseInt(this.updateInfo.newConfirmed)
+      this.eachCountryInfo[countryIndex].totConfirmed += parseInt(this.updateInfo.newConfirmed)
+      this.eachCountryInfo[countryIndex].totDead      += parseInt(this.updateInfo.newDead)
       this.initData()
       this.dataList.push(this.updateInfo.name + ":\n新增确诊：" + this.updateInfo.newConfirmed + "\n新增死亡：" + this.updateInfo.newDead + "\n新增治愈：" + this.updateInfo.newCured)
+      console.log(this.curCountry)
+      this.curCountry.countryName = this.updateInfo.name
+      this.curCountry.curNum      = this.eachCountryInfo[countryIndex].curConfirmed
+      this.curCountry.totNum      = this.eachCountryInfo[countryIndex].totConfirmed
+      this.curCountry.curedRate   = this.eachCountryInfo[countryIndex].curedRate
       this.drawer = false
     },
     getGlobalInfo() {
